64ea366682f70113efb41e877e3234885674f693
AleksandrGHub <bulanov.shura@gmail.com>
2024-06-05 22:23:21 +0300

    Добавьте файлы проекта.

---
+++ Degree two/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Degree_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degreeNumber = 0;
+            int randomNumber;
+            int divisor = 2;
+            double numberInPower = 1;
+            Random random = new Random();
+
+            randomNumber = random.Next(int.MaxValue);
+
+            for (int number = randomNumber; number >= 1; number /= divisor)
+            {
+                degreeNumber++;
+            }
+
+            for (int i = 0; i < degreeNumber; i++)
+            {
+                numberInPower *= divisor;
+            }
+
+            Console.WriteLine($"Произвольное число: {randomNumber}");
+            Console.WriteLine($"Степень числа {divisor}: {degreeNumber}");
+            Console.WriteLine($"Число в степени {divisor}: {numberInPower}");
+        }
+    }
+}
